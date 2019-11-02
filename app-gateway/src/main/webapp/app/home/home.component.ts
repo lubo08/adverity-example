9fa5156dfa1d415038c35c31123168da278d3664
lubo08 <lubo08@hotmail.com>
2019-11-02 19:31:54 +0100

    Front end changes

--- app-gateway/src/main/webapp/app/home/home.component.ts
+++ app-gateway/src/main/webapp/app/home/home.component.ts
@@ -6,6 +6,8 @@ import { JhiEventManager } from 'ng-jhipster';
 import { LoginModalService } from 'app/core/login/login-modal.service';
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/user/account.model';
+import { HttpClient } from '@angular/common/http';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'jhi-home',
@@ -17,17 +19,126 @@ export class HomeComponent implements OnInit, OnDestroy {
   authSubscription: Subscription;
   modalRef: NgbModalRef;
 
+  single: any[];
+  multi: any[] = [
+    {
+      name: 'Cyan',
+      series: [
+        {
+          name: 5,
+          value: 2650
+        },
+        {
+          name: 10,
+          value: 2800
+        },
+        {
+          name: 15,
+          value: 2000
+        }
+      ]
+    },
+    {
+      name: 'Yellow',
+      series: [
+        {
+          name: 5,
+          value: 2500
+        },
+        {
+          name: 10,
+          value: 3100
+        },
+        {
+          name: 15,
+          value: 2350
+        }
+      ]
+    }
+  ];
+
+  view: any[] = [700, 400];
+
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Date';
+  showYAxisLabel = true;
+  yAxisLabel = 'Impresions, Clicks';
+  timeline = true;
+  yAxis: [
+    {
+      type: 'linear';
+      labels: {
+        format: '{value} (c)';
+      };
+      title: {
+        text: 'Clicks';
+      };
+    },
+    {
+      type: 'linear';
+      labels: {
+        format: '{value} (i)';
+      };
+      title: {
+        text: 'Impresions';
+      };
+    }
+  ];
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  // line, area
+  autoScale = true;
+  dropdownList = [];
+
+  // model
+  selectedItems = [];
+  dropdownList2 = [];
+
+  // model
+  selectedItems2 = [];
+
+  // Example settings
+  dropdownSettings = {
+    dataIdProperty: 'idValue',
+    dataNameProperty: 'nameValue',
+    headerText: 'Test header',
+    noneSelectedBtnText: 'All selected',
+    btnWidth: 'auto',
+    dropdownHeight: 'auto',
+    showDeselectAllBtn: true,
+    showSelectAllBtn: true,
+    deselectAllBtnText: 'Deselect',
+    selectAllBtnText: 'Select',
+    btnClasses: 'btn btn-primary btn-sm dropdown-toggle',
+    selectionLimit: 3,
+    enableFilter: true
+  };
+
   constructor(
     private accountService: AccountService,
     private loginModalService: LoginModalService,
-    private eventManager: JhiEventManager
+    private eventManager: JhiEventManager,
+    private http: HttpClient
   ) {}
 
   ngOnInit() {
     this.accountService.identity().subscribe((account: Account) => {
       this.account = account;
+      this.loagData();
     });
     this.registerAuthenticationSuccess();
+  }
+
+  onSelect(event) {
+    // console.log(event);
   }
 
   registerAuthenticationSuccess() {
@@ -36,6 +147,56 @@ export class HomeComponent implements OnInit, OnDestroy {
         this.account = account;
       });
     });
+  }
+
+  loagData() {
+    this.http
+      .get<any[]>(`/api/_search/campaigns`)
+      .pipe(first())
+      .subscribe(response => {
+        response.forEach(source => {
+          this.dropdownList2.push({ idValue: source, nameValue: source });
+        });
+      });
+    this.http
+      .get<any[]>(`/api/_search/datasources`)
+      .pipe(first())
+      .subscribe(response => {
+        response.forEach(source => {
+          this.dropdownList.push({ idValue: source, nameValue: source });
+        });
+      });
+    this.http
+      .get<any[]>(`/api/_search/datagram`)
+      .pipe(first())
+      .subscribe(response => {
+        this.multi = [];
+        const clicks = [];
+        const impresions = [];
+        response.forEach(day => {
+          // console.log(day);
+          const lineDate = new Date(day.day);
+          clicks.push({
+            name: lineDate.toLocaleDateString(),
+            yAxis: 0,
+            value: day.clicks
+          });
+          impresions.push({
+            name: lineDate.toLocaleDateString(),
+            yAxis: 1,
+            value: day.impresions
+          });
+        });
+        this.multi.push({
+          name: 'Clicks',
+          series: clicks
+        });
+        this.multi.push({
+          name: 'Impresions',
+          series: impresions
+        });
+        // console.log(response);
+      });
   }
 
   isAuthenticated() {
--- app-gateway/src/main/webapp/app/home/home.module.ts
+++ app-gateway/src/main/webapp/app/home/home.module.ts
@@ -4,9 +4,11 @@ import { RouterModule } from '@angular/router';
 import { AadeSharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { NgxBootstrapMultiselectDropdownModule } from 'ngx-bootstrap-multiselect-dropdown';
 
 @NgModule({
-  imports: [AadeSharedModule, RouterModule.forChild([HOME_ROUTE])],
+  imports: [AadeSharedModule, RouterModule.forChild([HOME_ROUTE]), NgxChartsModule, NgxBootstrapMultiselectDropdownModule],
   declarations: [HomeComponent]
 })
 export class AadeHomeModule {}
